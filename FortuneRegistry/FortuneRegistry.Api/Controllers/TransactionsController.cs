b910895ca53fb00f6c5d9b151978aa8251a9c5b7
Alexander Yasko <a.yasko@pm.me>
2019-08-07 21:22:44 +0300

    Add Shared project

---
+++ FortuneRegistry/FortuneRegistry.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FortuneRegistry.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> GetAll()
+        {
+            return new List<string>()
+            {
+                "apple",
+                "orange",
+                "lemon"
+            };
+        }
+    }
+}
