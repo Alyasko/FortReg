761587c20ca31d46a9517b2d3ef8b8a59285246d
Alexander Yasko <a.yasko@kontrol.tech>
2019-09-14 15:47:33 +0300

    Midway

--- FortuneRegistry/FortuneRegistry.IOS/ViewController.cs
+++ FortuneRegistry/FortuneRegistry.IOS/ViewController.cs
@@ -1,6 +1,15 @@
 ﻿using Foundation;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 using FortuneRegistry.IOS.Model;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
 using UIKit;
 
 namespace FortuneRegistry.IOS
@@ -10,13 +19,13 @@ namespace FortuneRegistry.IOS
         private readonly CurrencyPickerModel _currencyPickerModel = new CurrencyPickerModel();
         private readonly CategoryPickerModel _categoryPickerModel = new CategoryPickerModel();
 
-        public ViewController (IntPtr handle) : base (handle)
+        public ViewController(IntPtr handle) : base(handle)
         {
         }
 
-        public override void ViewDidLoad ()
+        public override void ViewDidLoad()
         {
-            base.ViewDidLoad ();
+            base.ViewDidLoad();
 
             PickerCurrency.Model = _currencyPickerModel;
             PickerCategory.Model = _categoryPickerModel;
@@ -59,9 +68,9 @@ namespace FortuneRegistry.IOS
             //};
         }
 
-        public override void DidReceiveMemoryWarning ()
+        public override void DidReceiveMemoryWarning()
         {
-            base.DidReceiveMemoryWarning ();
+            base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
 
@@ -69,6 +78,82 @@ namespace FortuneRegistry.IOS
         {
             _categoryPickerModel.Names.Add("Test");
             PickerCategory.Model = _categoryPickerModel;
+
+            Test(null);
+        }
+
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = {SheetsService.Scope.SpreadsheetsReadonly};
+        static string ApplicationName = "Google Sheets API .NET Quickstart";
+
+        static void Test(string[] args)
+        {
+            UserCredential credential;
+
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(ViewController)).Assembly;
+            Stream stream1 = assembly.GetManifestResourceStream("FortuneRegistry.IOS.gcred.json");
+            string text = "";
+            using (var reader = new System.IO.StreamReader(stream1))
+            {
+                text = reader.ReadToEnd();
+            }
+
+
+
+            var rootFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            File.WriteAllText(Path.Combine(rootFolder, "hello.txt"), "Hello!");
+
+            var files = Directory.GetFiles(rootFolder);
+
+            using (var stream =
+                new FileStream("gcreds.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+            String range = "Class Data!A2:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                Console.WriteLine("Name, Major");
+                foreach (var row in values)
+                {
+                    // Print columns A and E, which correspond to indices 0 and 4.
+                    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+
+            Console.Read();
         }
     }
 }
