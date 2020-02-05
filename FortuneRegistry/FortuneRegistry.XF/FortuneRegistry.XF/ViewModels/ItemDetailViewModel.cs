63d3f553acf73b48a039719cbd5d340027e9f164
Alyasko <a.yasko@pm.me>
2020-02-05 19:27:52 +0100

    Add XF projects

---
+++ FortuneRegistry/FortuneRegistry.XF/FortuneRegistry.XF/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FortuneRegistry.XF.Models;
+
+namespace FortuneRegistry.XF.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
